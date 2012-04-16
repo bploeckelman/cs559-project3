d300eae11ca596fa75c65b789a311f0f40b6a040
Brian Ploeckelman <ploeckelman@wisc.edu>
2012-04-16 00:55:08 -0500

    Added Scene method comments

--- Scene/Scene.h
+++ Scene/Scene.h
@@ -27,18 +27,26 @@ public:
 	Scene();
 	~Scene();
 
+	// Load resources, arrange scene and cameras
 	void setup();
+	// Update this scene, process input
 	void update(const sf::Clock& clock, const sf::Input& input);
+	// Render this scene's objects from the current camera perspective
 	void render(const sf::Clock& clock);
 
+	// Draw a basic cube at the specified position with the specified scale
 	void renderTestCube(const glm::vec3& position=glm::vec3(0.f, 0.f, 0.f)
 					  , const float scale=1.f);
+	// Draw the specified plane as a quad with the specified radius
 	void renderTestPlane(const Plane& plane, const float radius=100.f);
 
+	// Peek at the camera position
 	const glm::vec3& cameraPosition() const;
 	
 private:
+	// Allocate any needed memory, called on creation
 	void init();
+	// Free any allocated memory, called on destruction
 	void cleanup();
 };
 
