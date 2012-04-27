6e47a2be3bea8f6f6837a242baf57cb399698109
Brian Ploeckelman <ploeckelman@wisc.edu>
2012-04-27 14:15:41 -0500

    Added header to include all particle system related headers

---
+++ Particles/Particles.h
@@ -0,0 +1,13 @@
+#pragma once
+/************************************************************************/
+/* Particles 
+/* ---------
+/* Include all particle system related headers 
+/************************************************************************/
+
+
+#include "Particle.h"
+#include "Emitters.h"
+#include "ParticleEmitter.h"
+#include "ParticleSystem.h"
+#include "ParticleManager.h"
